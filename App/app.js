af9b1bdd0476dadd27eff3a45591a0b2ed2028d8
JacksonRolando <47893454+JacksonRolando@users.noreply.github.com>
2020-11-02 13:53:29 -0600

    Test node server up

---
+++ App/app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const path = require('path')
+
+const {
+    PORT = 8000,
+
+} = process.env
+
+
+app = express()
+app.use(bodyParser.urlencoded({extended: false}))
+app.set('view engine', 'ejs')
+app.use(bodyParser.json()) //parse json data
+
+
+//Import javascript files
+const {getHomePage} = require('./routes/index.js')
+
+//defines requests by url
+app.get('/', getHomePage)
+
+
+app.listen(PORT, () => console.log("Server running on port " + PORT))
