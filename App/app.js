27ec5e47d8ced6b7e8853d9d042e7b1f1e0670b4
JacksonRolando <jprolando7762@gmail.com>
2020-11-30 00:20:23 -0600

    Thank the lord inputting files is working

--- App/app.js
+++ App/app.js
@@ -3,27 +3,49 @@ const bodyParser = require('body-parser')
 const session = require('express-session')
 const path = require('path')
 const MongoClient = require("mongodb").MongoClient
+//const ObjectId = require('mongodb').ObjectId
 const multer = require('multer')
 
 const FILE_DIR = "./public/data/uploads/"
 global.FILE_DIR = FILE_DIR
 const upload = multer({dest: FILE_DIR})
 
+const TWO_HOURS = 1000 * 60 * 60 * 2
 const {
     PORT = 8001,
-    NODE_ENV = 'development'
+    NODE_ENV = 'development',
+
+    SESS_SECRET = "This is a secret key to encrypt the session", //CHANGE THIS!!!
+    SESS_NAME = 'sid',
+    SESS_LIFETIME = TWO_HOURS
 } = process.env
 
+global.name = SESS_NAME
 
+const IN_PROD = NODE_ENV === 'production'
+
+//set up app
 app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 app.set('view engine', 'ejs')
 app.use(bodyParser.json()) //parse json data
 
+//set up session
+app.use(session({
+    name: SESS_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: SESS_SECRET,
+    cookie: {
+        maxAge: SESS_LIFETIME,
+        sameSite: true,
+        secure: IN_PROD
+    }
+}))
 
 //set database variables
 global.dburl = "mongodb://localhost:27017/onlineArchive"
-global.dbclient = new MongoClient(dburl, { useUnifiedTopology: true})
+
 
 
 //Import javascript files
@@ -35,7 +57,7 @@ const {inputFormPage, submitInputFile, chooseEventPage} = require('./routes/admi
 app.get('/', getHomePage)
 
 app.get('/inputFile', inputFormPage)
-//app.post('/inputFile', submitInputFile)
+app.post('/inputFile', submitInputFile)
 
 app.post('/chooseEvent', upload.single("fileImport"), chooseEventPage)
 
@@ -51,5 +73,7 @@ const {initialDbSetup} = require('./routes/testSetup.js')
 
 //First time run on new server
 app.get('/setup', initialDbSetup)
+
+
 
 app.listen(PORT, () => console.log("Server running on port " + PORT))
--- App/routes/admin.js
+++ App/routes/admin.js
@@ -1,4 +1,5 @@
 const { query } = require("express")
+const { MongoClient, ObjectID } = require("mongodb")
 
 module.exports = {
     inputFormPage : (req, res) => {
@@ -6,16 +7,10 @@ module.exports = {
     },
     chooseEventPage : (req, res) => {
         //takes in date from inputFile form and puts it into a Date object
-        year = parseInt(req.body.year)
-        month = parseInt(("0" + req.body.month).slice(-2))
-        day = parseInt(("0" + req.body.day).slice(-2))
-        fileDate = new Date(year, month - 1, day)
+        fileDate = module.exports.parseReqDate(req)
 
-        req.body.year = year
-        req.body.month = month
-        req.body.day = day
-
-        dbclient = global.dbclient
+        
+        dbclient = new MongoClient(global.dburl, { useUnifiedTopology: true})
         dbclient.connect((err, db) => {
             if(err){
                 console.log(err)
@@ -23,14 +18,16 @@ module.exports = {
             } 
             else{
                 var dbo = db.db("onlineArchive")
-                //query = {date: fileDate}
 
+                //console.log("filename: " + req.file.filename)
+                
                 fileObj = {
                     title: req.body.title,
                     creator: req.body.creator,
+                    fileType: req.body.fileType,
                     description: req.body.description,
                     date: fileDate,
-                    filePath: global.FILE_DIR + req.file
+                    filePath: global.FILE_DIR + req.file.filename
                 }
 
                 dbo.collection("files").insertOne(fileObj, (err, result) => {
@@ -39,32 +36,113 @@ module.exports = {
                         db.close()
                     }
                     else{
-                        console.log("1 row inserted:")
-                        console.log(result)
+                        //console.log("1 row inserted into files collection")
 
-                        db.close()
-                        res.redirect('/')
+                        req.session.fileId = result.insertedId
+                        
 
-                        //TODO: Make picture save as jpg?, Save correct file path to database
-
-                        /*dbo.collection("events").find({date: fileDate}).toArray((err, result) => {
+                        dbo.collection("events").find({date: fileDate}).toArray((err, result) => {
                             if(err) {
                                 console.log(err)
                                 db.close()
                             }
                             else{
-                                console.log(result)
                                 events = result
                                 db.close()
-                                console.log(req.body)
-                                console.log(events)
-                                res.render("chooseEvent", {file : req.body, events : events})
+                                res.render("chooseEvent", {year: year, month: month, day: day, events: events})
                             }
-                        })*/
+                        })
                     }
                 })
                     
             }
         })
+    },
+    submitInputFile: (req, res) => {
+        if(req.body.eventType == 'newEvent'){
+            dbclient = new MongoClient(global.dburl, {useUnifiedTopology: true})
+            dbclient.connect((err, db) => {
+                if(err) {
+                    console.log(err)
+                    db.close()
+                }
+                else{
+                    dbo = db.db('onlineArchive')
+
+                    dateObj = module.exports.parseReqDate(req)
+
+                    eventObj = {
+                        title: req.body.eventTitle,
+                        description: req.body.eventDescription,
+                        date: dateObj,
+                        location: req.body.location
+                    }
+
+                    dbo.collection("events").insertOne(eventObj, (err, result) => {
+                        if(err) {
+                            console.log(err)
+                            db.close()
+                        }
+                        else {
+                            eventId = result.insertedId
+
+                            const ObjectId = require('mongodb').ObjectId
+
+                            myQuery = {_id: new ObjectId(req.session.fileId)}
+                            values = {$set: {eventId: new ObjectId(eventId), date: dateObj}}
+
+                            dbo.collection("files").updateOne(myQuery, values, (err, result) => {
+                                if(err) {
+                                    console.log(err)
+                                    db.close()
+                                }
+                                else{
+                                    db.close()
+                                    res.redirect("/")
+                                }
+                            })
+
+                        }
+                    })
+                }
+            })
+        }
+        if(req.body.eventType == 'existing'){
+            eventId = req.body.event
+            
+            dbclient = new MongoClient(global.dburl, {useUnifiedTopology: true})
+            dbclient.connect((err, db) => {
+                if(err) {
+                    console.log(err)
+                    db.close()
+                }
+                else{
+                    dbo = db.db('onlineArchive')
+
+                    const ObjectId = require('mongodb').ObjectId
+
+                    myQuery = {_id: new ObjectId(req.session.fileId)}
+                    values = {$set: {eventId: new ObjectId(eventId)}}
+
+                    dbo.collection('files').updateOne(myQuery, values, (err, result) => {
+                        if(err) {
+                            console.log(err)
+                            db.close()
+                        }
+                        else{
+                            db.close()
+                            res.redirect("/")
+                        }
+                    })
+                }
+            })
+        }
+    },
+    parseReqDate: (req) => {
+        year = parseInt(req.body.year)
+        month = parseInt(("0" + req.body.month).slice(-2))
+        day = parseInt(("0" + req.body.day).slice(-2))
+        dateObj = new Date(year, month - 1, day)
+        return dateObj
     }
 }
